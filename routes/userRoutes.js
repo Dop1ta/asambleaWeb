4961dbb6385795d71520f88278ab549e52f3d6b4
Dop1ta <gabrielrburgos@outlook.com>
2022-11-12 21:55:29 -0300

    r

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const api = express.Route();
+const api = express.Router();
 
 api.post("/createUser", userController.createUser);
 api.get("/getUsers", userController.getUsers);
