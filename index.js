8723a92ebd59593d273feb4567d17a94304c772c
DiegoNancu <d_nancupillan@hotmail.com>
2022-11-12 21:07:37 -0300

    test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.DB, options, (error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Connected to database");
+    console.log("Connected to database123");
   }
 });
 
