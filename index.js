22ae21765c5d608b66ebf32ba18a2fd8cf63eb99
ManuelQuijada <manuel.quijada1901@alumnos.ubiobio.cl>
2022-11-28 00:21:35 -0300

    fix

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const programMeetingRoutes = require("./routes/program_meetingRoutes");
 const actaRoutes = require("./routes/actaRoutes");
 const userRoutes = require("./routes/userRoutes");
 const votingActivityRoutes = require("./routes/votingActivityRoutes");
-// const fileRoutes = require("./routes/fileRoutes");
-// const foroRoutes = require(".routes/foroRoutes");
+const fileRoutes = require("./routes/fileRoutes");
+const foroRoutes = require(".routes/foroRoutes");
 
 app.use(cors());
 app.use(express.json());
@@ -19,8 +19,8 @@ app.use("/api", programMeetingRoutes);
 app.use("/api", actaRoutes);
 app.use("/api", userRoutes);
 app.use("/api", votingActivityRoutes);
-// app.use("/api", fileRoutes);
-// app.use("/api", foroRoutes);
+app.use("/api", fileRoutes);
+app.use("/api", foroRoutes);
 
 const options = {
   useNewUrlParser: true,
