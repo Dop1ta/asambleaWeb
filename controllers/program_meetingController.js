973471e0f1aef1659866b1551446f4e30c7b8adc
Dop1ta <gabrielrburgos@outlook.com>
2022-11-28 01:14:52 -0300

    xd

--- controllers/program_meetingController.js
+++ controllers/program_meetingController.js
@@ -38,30 +38,30 @@ const createMeeting = (req, res) => {
         if (error) {
           return res.status(400).send({ message: "Error creando reunión." });
         }
-        // try {
-        //   user.find({}, (error, person) => {
-        //     let directory = person.map((person) => person.email);
-        //     const mailOptions = {
-        //       from: `Administrador`,
-        //       to: directory,
-        //       subject: "Nueva reuinon agendada",
-        //       text: `Hola, se ha realizado de forma correcta el envio de los correos`,
-        //       html: `
-        //             <h2>Hola estimados vecinos, se a agendado una reunion </h2>
-        //             <p>Dia: ${meeting.time}, Hora: ${meeting.hour}, lugar: ${meeting.place}</p>
-        //             <a href="http://146.83.198.35:1203/api/getMeetings/search/${meeting._id}"> Para mas informacion </a>
-        //         `,
-        //     };
-        //     transporter.sendMail(mailOptions, (err, info) => {
-        //       if (err) {
-        //         return res.status(400).send({ message: "Error al enviar el correo" });
-        //       }
-        //       return res.status(200).send({ message: "Mensaje enviado" });
-        //     });
-        //   });
-        // } catch (error) {
-        //   return res.status(400).send({ message: "Error enviando correo." });
-        // }
+        try {
+          user.find({}, (error, person) => {
+            let directory = person.map((person) => person.email);
+            const mailOptions = {
+              from: `Administrador`,
+              to: directory,
+              subject: "Nueva reuinon agendada",
+              text: `Hola, se ha realizado de forma correcta el envio de los correos`,
+              html: `
+                    <h2>Hola estimados vecinos, se a agendado una reunion </h2>
+                    <p>Dia: ${meeting.time}, Hora: ${meeting.hour}, lugar: ${meeting.place}</p>
+                    <a href="http://146.83.198.35:1203/api/getMeetings/search/${meeting._id}"> Para mas informacion </a>
+                `,
+            };
+            transporter.sendMail(mailOptions, (err, info) => {
+              if (err) {
+                return res.status(400).send({ message: "Error al enviar el correo" });
+              }
+              return res.status(200).send({ message: "Mensaje enviado" });
+            });
+          });
+        } catch (error) {
+          return res.status(400).send({ message: "Error enviando correo." });
+        }
         return res.status(201).send(meeting);
       });
     } else {
