747d83817a4cab2e7e84de27dc3e5ed35ded6316
Manu <manuel.quijada1901@alumnos.ubiobio.cl>
2022-12-29 03:29:15 -0300

    fix

--- frontend/pages/Acta.jsx
+++ frontend/pages/Acta.jsx
@@ -1,20 +1,65 @@
-import { useState } from 'react'
-import { Stack, Button, text} from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import { Stack, Button, Text, Card, CardHeader, Heading, CardBody, SimpleGrid} from '@chakra-ui/react'
+import axios from 'axios'
 import { AiOutlinePlus } from "react-icons/md"
+import { useRouter } from 'next/router'
+import NavTabAdmin from '../components/NavTabAdmin'
 
 const Acta = () => {
 
     const [Actas, setActas] = useState([])
 
-    const getActa = async
+    const router = useRouter()
 
+    const getActa = async () => {
+        const response = await axios.get(`${process.env.API_URL}/getActa/639a48dffe299c865e0ea1f9`)
+        setActas(response.data)
+    }
 
-    //const getActa 
+    const createActa = () => {
+        router.push('/create/create_Acta')
+    }
+
+    useEffect(() => {
+        getActa()
+    }, [])
+
+    const showActas = () => {
+        if (Acta.length === 0) {
+            return (
+                <Card boxShadow='md' ml={30} my={4} variant='outline' overflow='hidden' alignItems='center' borderRadius={20} backgroundColor={"white"}>
+                    <CardHeader>
+                        <Heading size='md'>Sin Actas</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Text>No se ha encontrado ninguna Acta</Text>
+                    </CardBody>
+                </Card>
+            )
+        }else{
+            return Actas.map(Actap => {
+                return(
+                    <Card key={Actap._id} boxShadow='lg' ml={30} my={4} variant='outline' overflow='hidden' alignItems='center' borderRadius={20} backgroundColor={"white"}>
+                        <CardHeader>
+                            <Heading size='md'>{Actap.name}</Heading>
+                            <Text>{Actap.description}</Text>
+                            <Text>{Acta.date}</Text>
+                        </CardHeader>
+                    </Card>
+                )
+            })
+        }
+    }
     return (
-        <Stack spacing={4} direction='row' align='center'>
-            <Button lefticon={<AiOutlinePlus/>}colorScheme='yellow' size='md'>
-                Crear Acta
-            </Button>
+        <Stack alignItems={'center'} textAlign={'center'} backgroundColor={"rgb(244,247,254)"}>
+            <NavTabAdmin/>
+            <Heading textAlign={'center'} ml={30} my={4}>Actas</Heading>
+            <Stack ml={30} my={4}>
+                <Button lefticon={<AiOutlinePlus/>} onClick={createActa} size='md'>Crear Acta</Button>
+            </Stack>
+            <SimpleGrid>
+                {showActas()}
+            </SimpleGrid>
         </Stack>
     )
 }
