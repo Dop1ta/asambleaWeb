cb5f89ab3c65950cb11f4e64cb71db5a6b776d9e
Manu <manuel.quijada1901@alumnos.ubiobio.cl>
2022-12-31 09:21:43 -0300

    add

---
+++ frontend/components/files_upload.jsx
@@ -0,0 +1,40 @@
+import { useState,  } from 'react';
+import axios from 'axios';
+
+const file_upload = () => {
+
+    let [up, setUpload] = useState({
+        url: '',
+        name: '',
+        mimeType: ''
+    })
+
+    const upload = async () => {
+        try{
+            const response = await axios.post(`${process.env.API_URL}/uploadfile`)
+            setUpload(response.data)
+        }catch (error){
+        }
+    }
+
+    const onChange = async () => {
+        
+    }
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <form onSubmit={upload}>
+                        <div className="form-group">
+                            <input type="file" onChange={onChange} />
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary" type="submit">Subir Archivo</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+}
+
+export default file_upload
