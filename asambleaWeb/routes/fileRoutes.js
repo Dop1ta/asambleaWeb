fc800c7f78c10d03264787cbeb0cdcd1059d92eb
manu <manuel.quijada1901@alumnos.ubiobio.cl>
2023-01-03 07:33:38 -0300

    fix

--- asambleaWeb/routes/fileRoutes.js
+++ asambleaWeb/routes/fileRoutes.js
@@ -10,5 +10,6 @@ api.get('/files', fileController.getFiles);
 api.get('/files/get/:idacta', fileController.getFilesByActaId);
 api.get('/file/download/:idacta', fileController.getSFiles);
 api.delete('/file/delete/:idacta', fileController.deleteFiles);
+api.delete('/file/deletes/:id', fileController.deleteFilesS);
 
 module.exports = api;
